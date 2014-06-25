e9ca6da6e730f0dbf954c3c7f39dae831a140050
doomsayerz <sergey@doomsayer.tk>
2014-06-25 22:44:36 +0300

    my shopping cart

--- e_shop_full.js
+++ e_shop_full.js
@@ -3,24 +3,28 @@ var myShop = {
     availGoods: [
         {
             name: "Фен, 1г.",
-            price: "$100",
+            price: 100,
             availNum: 3
         },
         {
             name: "Ширево, 1г.",
-            price: "$70",
+            price: 70,
             availNum: 4
         },
         {
             name: "Колеса",
-            price: "$90",
+            price: 90,
             availNum: 0
         }
     ],
+
+    shoppingCart: [],
+
     init: function () {
-       this.displayItems();
+        this.displayItems();
     },
     displayItems: function () {
+        var self = this;
         for (var i = 0; i < this.availGoods.length; i++) {
             var $tr = document.createElement('tr'),
                 $td = document.createElement('td'),
@@ -37,13 +41,40 @@ var myShop = {
             $td.class = "buyButt";
             $control.type = "button";
             $control.value = "Купить";
-            $control.id = "purchase-button" + i;
-            if (this.availGoods.availNum == 0) {
-                $control.disabled = disabled;
+            $control.itemId = i;
+            if (this.availGoods[i].availNum == 0) {
+                $control.disabled = "disabled";
             }
+            $control.addEventListener('click', function () {
+                self.addGood(this.itemId);
+            }, false);
             $td.appendChild($control);
             $tr.appendChild($td);
             $table.appendChild($tr);
+        }
+    },
+
+    addGood: function (itemID) {
+        var i = this.findId(itemID);
+        if (i>=0) {
+            this.shoppingCart[i].addedNum++;
+            return;
+        };
+        var addedGood = {
+            name: this.availGoods[itemID].name,
+            price: this.availGoods[itemID].price,
+            addedNum: 1,
+            id: itemID
+        };
+        this.shoppingCart.push(addedGood);
+        console.log(this.shoppingCart);
+    },
+
+    findId: function(itemID) {
+        for (var i = 0; i  < this.shoppingCart.length; i++) {
+            if (this.shoppingCart[i].id == itemID) {
+                return i;
+            }
         }
     }
 }
